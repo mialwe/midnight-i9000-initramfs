a7b06005e58dbc61ac466f2f961ba206f71639b5
Michael Weingärtner <mialwe@googlemail.com>
2011-08-12 00:12:49 +0200

    reworked various tweaks again

--- sbin/post-init.sh
+++ sbin/post-init.sh
@@ -38,18 +38,6 @@ else
   /sbin/busybox rm /lib/modules/cifs.ko
 fi
 
-# Tweak cfq io scheduler
-  for i in $(/sbin/busybox_disabled ls -1 /sys/block/stl*) $(/sbin/busybox_disabled ls -1 /sys/block/mmc*)
-  do echo "0" > $i/queue/rotational
-    echo "0" > $i/queue/iostats
-    echo "1" > $i/queue/iosched/group_isolation
-    echo "4" > $i/queue/iosched/quantum
-    echo "1" > $i/queue/iosched/low_latency
-    echo "10" > $i/queue/iosched/slice_idle
-    echo "2" > $i/queue/iosched/back_seek_penalty
-    echo "1000000000" > $i/queue/iosched/back_seek_max
-  done
-
 # Remount all partitions with noatime
   for k in $(/sbin/busybox_disabled mount | /sbin/busybox_disabled grep relatime | /sbin/busybox_disabled cut -d " " -f3)
   do
@@ -65,26 +53,23 @@ fi
   done
   
 # Miscellaneous tweaks
-  echo "3000" > /proc/sys/vm/dirty_writeback_centisecs # Speedmod: 2000
-  echo "2000" > /proc/sys/vm/dirty_expire_centisecs    # Speedmod: 1000
+  echo "6000" > /proc/sys/vm/dirty_writeback_centisecs # Speedmod: 2000
+  echo "6000" > /proc/sys/vm/dirty_expire_centisecs    # Speedmod: 1000
   echo "0" > /proc/sys/vm/swappiness
 
 ################################################################################
 # MIDNIGHT ADDITIONS
 ################################################################################
 
-# Midnight: Diasbled as NOOP is used. Speedmod: Ondemand CPU governor tweaks
+# Midnight: Diasbled as CONSERVATIVE is used. Speedmod: Ondemand CPU governor tweaks
 #  echo "50" > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
 #  echo "80000" > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
 
 # /proc/sys/vm
 # nice, short documentation: http://www.linuxinsight.com/proc_sys_vm_hierarchy.html
-  echo "60" > /proc/sys/vm/dirty_background_ratio
-  echo "95" > /proc/sys/vm/dirty_ratio
+  echo "25" > /proc/sys/vm/dirty_ratio
+  echo "15" > /proc/sys/vm/dirty_background_ratio
   echo "5"  > /proc/sys/vm/laptop_mode
-
-# lowmemorykiller, using JUWEs' settings for now
-  echo "2560,4096,6144,12288,14336,18432" > /sys/module/lowmemorykiller/parameters/minfree
 
 # kill the tasks causing full memory
   sysctl -w vm.oom_kill_allocating_task=1
@@ -92,40 +77,30 @@ fi
 # more mem to cache fs dentry and inode to save cpu
   sysctl -w vm.vfs_cache_pressure=10
 
-# kernel scheduler tweaking
+# prop modifications
+  setprop debug.sf.hw 1
+  setprop wifi.supplicant_scan_interval 180
+  setprop windowsmgr.max_events_per_sec 180
+
+# kernel tweaks
+  echo "8" > /proc/sys/vm/page-cluster;
+  echo "10" > /proc/sys/fs/lease-break-time;
+  echo "2048" > /proc/sys/kernel/msgmni
+  echo "64000" > /proc/sys/kernel/msgmax
+  echo "268435456" > /proc/sys/kernel/shmmax
+  echo "500 512000 64 2048" > /proc/sys/kernel/sem
   sysctl -w kernel.sched_latency_ns=600000
   sysctl -w kernel.sched_min_granularity_ns=400000
-  syscte -w kernel.sched_wakeup_granularity_ns=1000000
-  #sysctl -w kernel.sched_features=24188
+  sysctl -w kernel.sched_wakeup_granularity_ns=400000
+  echo NO_NORMALIZED_SLEEPER > /sys/kernel/debug/sched_features
 
-# prop modifications
-setprop debug.sf.hw 1
-setprop wifi.supplicant_scan_interval 180
-setprop windowsmgr.max_events_per_sec 180
+# noop scheduler tweak
+  for i in $(/sbin/busybox_disabled ls -1 /sys/block/stl*) $(/sbin/busybox_disabled ls -1 /sys/block/mmc*)
+  do 
+    echo "248" > $i/queue/nr_requests
+  done
 
-#internet speed tweaks (http://forum.xda-developers.com/showthread.php?t=1205259)
-  echo 0 > /proc/sys/net/ipv4/tcp_timestamps;
-  echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse;
-  echo 1 > /proc/sys/net/ipv4/tcp_sack;
-  echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle;
-  echo 1 > /proc/sys/net/ipv4/tcp_window_scaling;
-  echo 5 > /proc/sys/net/ipv4/tcp_keepalive_probes;
-  echo 30 > /proc/sys/net/ipv4/tcp_keepalive_intvl;
-  echo 30 > /proc/sys/net/ipv4/tcp_fin_timeout;
-  echo 404480 > /proc/sys/net/core/wmem_max;
-  echo 404480 > /proc/sys/net/core/rmem_max;
-  echo 256960 > /proc/sys/net/core/rmem_default;
-  echo 256960 > /proc/sys/net/core/wmem_default;
-  echo 4096 16384 404480 > /proc/sys/net/ipv4/tcp_wmem;
-  echo 4096 87380 404480 > /proc/sys/net/ipv4/tcp_rmem;
-
-#kernel tweaks (http://forum.xda-developers.com/showthread.php?t=1205259)
-  echo "8" > /proc/sys/vm/page-cluster;
-  echo "64000" > /proc/sys/kernel/msgmni;
-  echo "64000" > /proc/sys/kernel/msgmax;
-  echo "10" > /proc/sys/fs/lease-break-time;
-  echo 500 512000 64 2048 > /proc/sys/kernel/sem;
-
+# END MIDNIGHT ADDITIONS
 ################################################################################
 
 # SD cards (mmcblk) read ahead tweaks
