caee9ee143edd6491c5557461d08cdf968720326
Michael Weingärtner <mialwe@googlemail.com>
2011-10-08 15:51:27 +0200

    some more post-init.sh tweaks...

--- sbin/post-init.sh
+++ sbin/post-init.sh
@@ -735,7 +735,61 @@ echo "Applying IO scheduler settings";
 echo "4x '-1...' is ok here..."
 for i in $(ls -1 /sys/block/stl*) $(ls -1 /sys/block/mmc*) $(ls -1 /sys/block/bml*) $(ls -1 /sys/block/tfsr*); do echo $SCHEDULER > $i/queue/scheduler;done;
 
+#######################################################################
+# EXPERIMENTAL TWEAKS FROM XDA/PIKACHU/PLATYPUS...
+#######################################################################
+STL=`ls -d /sys/block/stl*`;
+BML=`ls -d /sys/block/bml*`;
+MMC=`ls -d /sys/block/mmc*`;
 
+for i in $STL $BML $MMC; 
+do                                       
+	echo 0 > $i/queue/rotational;               
+    echo 0 > $k/queue/iostats;
+    echo 1 > $i/queue/rq_affinity;                               
+done;
+
+echo 0 > /proc/sys/vm/laptop_mode;
+
+echo 0 > /proc/sys/net/ipv4/tcp_timestamps;
+echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse;
+echo 1 > /proc/sys/net/ipv4/tcp_sack;
+echo 1 > /proc/sys/net/ipv4/tcp_dsack;
+echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle;
+echo 1 > /proc/sys/net/ipv4/tcp_window_scaling;
+echo 5 > /proc/sys/net/ipv4/tcp_keepalive_probes;
+echo 30 > /proc/sys/net/ipv4/tcp_keepalive_intvl;
+echo 30 > /proc/sys/net/ipv4/tcp_fin_timeout;
+echo 524288 > /proc/sys/net/core/wmem_max;
+echo 524288 > /proc/sys/net/core/rmem_max;
+
+sysctl -w net.ipv4.tcp_mem="57344 57344 65536";
+
+echo 1 > /proc/sys/net/ipv4/tcp_moderate_rcvbuf 
+echo 256960 > /proc/sys/net/core/rmem_default;
+echo 256960 > /proc/sys/net/core/wmem_default;
+echo 4096 16384 404480 > /proc/sys/net/ipv4/tcp_wmem;
+echo 4096 87380 404480 > /proc/sys/net/ipv4/tcp_rmem;
+setprop net.tcp.buffersize.default 4096,87380,256960,4096,16384,256960;
+setprop net.tcp.buffersize.wifi 4095,87380,256960,4096,16384,256960;
+setprop net.tcp.buffersize.umts 4094,87380,256960,4096,16384,256960;
+
+echo "1" >  /proc/sys/net/ipv6/icmp_echo_ignore_broadcasts; # default
+echo "1" >  /proc/sys/net/ipv6/icmp_echo_ignore_all; # default: 0
+echo "1" >  /proc/sys/net/ipv6/icmp_ignore_bogus_error_responses;
+echo 4096 > /proc/sys/net/ipv4/tcp_max_syn_backlog; # default: 128
+echo 2 > /proc/sys/net/ipv4/tcp_synack_retries; # default: 5
+
+#media.stagefright.enable-player=true
+#media.stagefright.enable-meta=true
+#media.stagefright.enable-scan=true
+#media.stagefright.enable-http=true
+
+echo "NO_GENTLE_FAIR_SLEEPERS" > /sys/kernel/debug/sched_features;
+echo "NO_NEW_FAIR_SLEEPERS" > /sys/kernel/debug/sched_features;
+echo "NO_NORMALIZED_SLEEPER" > /sys/kernel/debug/sched_features;
+
+#######################################################################
 # END MIDNIGHT ADDITIONS
 ############################
 
@@ -877,4 +931,5 @@ mount|grep /system
 mount|grep /data
 mount|grep /dbdata
 mount|grep /cache
+
 echo "Ok, lets start Android... bye."
