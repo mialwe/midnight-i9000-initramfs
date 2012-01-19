2540ab678d70a8a27f8c35e14b37340189d9804a
Michael Weingärtner <mialwe@googlemail.com>
2012-01-19 21:00:00 +0100

    bugfix: init.d switch, cosmetics, cleanup

--- sbin/boot/35_misc.sh
+++ sbin/boot/35_misc.sh
@@ -24,8 +24,6 @@ if /sbin/busybox [ ! -f /cache/midnight_block ];then
         uv200=`/sbin/busybox awk -F"\"" ' /uv_200\"/ {print $4}' $xmlfile`;#uv200=$(($uv200*(-1)))
         uv100=`/sbin/busybox awk -F"\"" ' /uv_100\"/ {print $4}' $xmlfile`;#uv100=$(($uv100*(-1)))
         echo "APP: uv at boot -> $uvatboot"
-        echo "APP: uv1300 -> $uv1300"
-        echo "APP: uv1200 -> $uv1200"
         echo "APP: uv1000 -> $uv1000"
         echo "APP: uv800  -> $uv800"
         echo "APP: uv400  -> $uv400"
@@ -447,48 +445,41 @@ cat /proc/meminfo|grep ^Cached
 echo
 echo "INIT.D: starting..."
 # init.d support 
-CONFFILE="midnight_misc.conf"
-if /sbin/busybox [ initd=="true" ];then
-    if /sbin/busybox [ "`grep INIT_D /system/etc/midnight/$CONFFILE`" ]; then
-        echo $(date) USER EARLY INIT START from /system/etc/init.d
-        if cd /system/etc/init.d >/dev/null 2>&1 ; then
-            for file in E* ; do
-                if ! cat "$file" >/dev/null 2>&1 ; then continue ; fi
-                echo "INIT.D: START '$file'"
-                /system/bin/sh "$file"
-                echo "INIT.D: EXIT '$file' ($?)"
-            done
-        fi
-        echo $(date) USER EARLY INIT DONE from /system/etc/init.d
-
-        echo $(date) USER INIT START from /system/etc/init.d
-        if cd /system/etc/init.d >/dev/null 2>&1 ; then
-            for file in S* ; do
-                if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
-                echo "INIT.D: START '$file'"
-                /system/bin/sh "$file"
-                echo "INIT.D: EXIT '$file' ($?)"
-            done
-        fi
-        echo $(date) USER INIT DONE from /system/etc/init.d
-
-        echo $(date) USER INIT START from /system/etc/init.d
-        if cd /system/etc/init.d >/dev/null 2>&1 ; then
-            for file in [0-9][0-9]* ; do
-                if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
-                echo "INIT.D: START '$file'"
-                /system/bin/sh "$file"
-                echo "INIT.D: EXIT '$file' ($?)"
-            done
-        fi
-        echo $(date) USER INIT DONE from /system/etc/init.d
-    else
-        echo "INIT.D: disabled..."
+if /sbin/busybox [ "$initd" == "true" ];then
+    echo $(date) USER EARLY INIT START from /system/etc/init.d
+    if cd /system/etc/init.d >/dev/null 2>&1 ; then
+        for file in E* ; do
+            if ! cat "$file" >/dev/null 2>&1 ; then continue ; fi
+            echo "INIT.D: START '$file'"
+            /system/bin/sh "$file"
+            echo "INIT.D: EXIT '$file' ($?)"
+        done
     fi
-echo
-    echo "--------------------------------------------------------------------"                
+    echo $(date) USER EARLY INIT DONE from /system/etc/init.d
+
+    echo $(date) USER INIT START from /system/etc/init.d
+    if cd /system/etc/init.d >/dev/null 2>&1 ; then
+        for file in S* ; do
+            if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
+            echo "INIT.D: START '$file'"
+            /system/bin/sh "$file"
+            echo "INIT.D: EXIT '$file' ($?)"
+        done
+    fi
+    echo $(date) USER INIT DONE from /system/etc/init.d
+
+    echo $(date) USER INIT START from /system/etc/init.d
+    if cd /system/etc/init.d >/dev/null 2>&1 ; then
+        for file in [0-9][0-9]* ; do
+            if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
+            echo "INIT.D: START '$file'"
+            /system/bin/sh "$file"
+            echo "INIT.D: EXIT '$file' ($?)"
+        done
+    fi
+    echo $(date) USER INIT DONE from /system/etc/init.d
+    echo
     echo "LOG: VALUES *AFTER* INIT.D EXECUTION:"
-    echo "--------------------------------------------------------------------"                
     echo "MOUNT: Check mounted partitions: "
     /sbin/busybox mount|grep /system
     /sbin/busybox mount|grep /data
@@ -571,10 +562,7 @@ echo
     cat /proc/meminfo|grep ^MemFree
     cat /proc/meminfo|grep ^Buffers
     cat /proc/meminfo|grep ^Cached
-    echo "--------------------------------------------------------------------"                
     echo "LOG: VALUES *AFTER* INIT.D END"
-    echo "--------------------------------------------------------------------"                
-
 else 
     echo "INIT.D: disabled..."
 fi
@@ -588,5 +576,3 @@ echo "disabling /sbin/busybox, using /system/xbin/busybox now..."
 
 echo "mounting rootfs readonly..."
 /sbin/busybox_disabled mount -t rootfs -o remount,ro rootfs;
-
-
